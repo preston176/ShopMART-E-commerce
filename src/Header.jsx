5b607606c3d4c06c864279d940f307349d61aeb6
preston176 <prestonnyamweya@gmail.com>
2023-07-24 01:49:50 +0300

    Header logo

--- src/Header.jsx
+++ src/Header.jsx
@@ -4,7 +4,7 @@ import './Header.css'
 const Header = () => {
   return (
     <div className="header">
-      
+      <img className='header--logo' src="https://thumbs.dreamstime.com/b/lets-shopping-logo-design-template-cart-icon-designs-134743663.jpg" alt="store-logo" />
     </div>
   )
 }
