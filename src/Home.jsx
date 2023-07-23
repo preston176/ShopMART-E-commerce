eb18dc84fb1d5c13a86a42d20da6987366cfca0a
preston176 <prestonnyamweya@gmail.com>
2023-07-24 02:28:09 +0300

    banner added

--- src/Home.jsx
+++ src/Home.jsx
@@ -3,7 +3,13 @@ import './home.css'
 const Home = () => {
   return (
     <div className="home">
-      
+    <div className="home__container">
+        <img
+            className="home__image"
+            src="./images/banner.jpg"
+            alt="home-image"
+        />
+    </div>
     </div>
   )
 }
