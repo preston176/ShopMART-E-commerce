f400db5ccf9f72d63f3b9916e8ca465b86641e66
preston176 <prestonnyamweya@gmail.com>
2023-07-24 01:38:43 +0300

    firebase

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCf4MNT60BFlgF0MqmYLULaGbSD2HuBqi4",
+    authDomain: "markethub-f6f5e.firebaseapp.com",
+    projectId: "markethub-f6f5e",
+    storageBucket: "markethub-f6f5e.appspot.com",
+    messagingSenderId: "931912063437",
+    appId: "1:931912063437:web:0d04ad8ea6fe30ba674c84"
+  };
