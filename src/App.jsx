3ba35a7474add0db50e58dc3ecac041a9d0f0cbd
preston176 <prestonnyamweya@gmail.com>
2023-07-24 02:23:27 +0300

    home component created

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import "./App.css";
 import Header from "./Header";
+import Home from "./Home";
 
 const App = () => {
   return (
     <div className="app">
       <Header />
-      {/* Home */}
+      <Home />
     </div>
   );
 };
