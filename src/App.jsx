10ad744ebc56dac6ba9a1e493699bd2d3e94c233
preston176 <prestonnyamweya@gmail.com>
2023-07-24 02:04:47 +0300

    logo size fix

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,6 @@ const App = () => {
 
   return (
     <div className='app'>
-      <h1></h1>
       <Header />
       {/* Home */}
     </div>
--- src/Header.jsx
+++ src/Header.jsx
@@ -5,7 +5,7 @@ import SearchIcon from '@mui/icons-material/Search';
 const Header = () => {
   return (
     <div className="header">
-      <img className='header--logo' src="https://thumbs.dreamstime.com/b/lets-shopping-logo-design-template-cart-icon-designs-134743663.jpg" alt="store-logo" />
+      <img className='header__logo' src="https://thumbs.dreamstime.com/b/lets-shopping-logo-design-template-cart-icon-designs-134743663.jpg" alt="store-logo" />
 
       <div className="header__search">
 
