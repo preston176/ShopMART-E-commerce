4cb040f5e3c1a4e608fe0e4594697a7ba290d6b1
preston176 <prestonnyamweya@gmail.com>
2023-07-24 02:08:12 +0300

    prettier

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,13 @@
-import './App.css'
-import Header from './Header'
+import "./App.css";
+import Header from "./Header";
 
 const App = () => {
-
-
   return (
-    <div className='app'>
+    <div className="app">
       <Header />
       {/* Home */}
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- src/Header.jsx
+++ src/Header.jsx
@@ -1,46 +1,36 @@
-import './Header.css'
-import SearchIcon from '@mui/icons-material/Search';
-
+import "./Header.css";
+import SearchIcon from "@mui/icons-material/Search";
 
 const Header = () => {
   return (
     <div className="header">
-      <img className='header__logo' src="https://thumbs.dreamstime.com/b/lets-shopping-logo-design-template-cart-icon-designs-134743663.jpg" alt="store-logo" />
+      <img
+        className="header__logo"
+        src="https://thumbs.dreamstime.com/b/lets-shopping-logo-design-template-cart-icon-designs-134743663.jpg"
+        alt="store-logo"
+      />
 
       <div className="header__search">
-
-        <input className='header__searchInput' type="text" />
+        <input className="header__searchInput" type="text" />
         <SearchIcon />
       </div>
 
-        <div className="header__nav">
-            <div className="header__option">
-                <span className='header__optionLine1'>
-                    Hello User
-                </span>
-                <span className='header__optionLine2'>
-                    Log in
-                </span>
-            </div>
-            <div className="header__option">
-            <span className='header__optionLine1'>
-                    Requset returns
-                </span>
-                <span className='header__optionLine2'>
-                    & Orders
-                </span>
-            </div>
-            <div className="header__option">
-            <span className='header__optionLine1'>
-                    Your
-                </span>
-                <span className='header__optionLine2'>
-                    Pro Subscription
-                </span>
-            </div>
+      <div className="header__nav">
+        <div className="header__option">
+          <span className="header__optionLine1">Hello User</span>
+          <span className="header__optionLine2">Log in</span>
         </div>
+        <div className="header__option">
+          <span className="header__optionLine1">Requset returns</span>
+          <span className="header__optionLine2">& Orders</span>
+        </div>
+        <div className="header__option">
+          <span className="header__optionLine1">Your</span>
+          <span className="header__optionLine2">Pro Subscription</span>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
