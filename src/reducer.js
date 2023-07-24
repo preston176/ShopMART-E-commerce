3db9d5e1453c4b50c0f8c8380e6a131aa6239d47
preston176 <prestonnyamweya@gmail.com>
2023-07-24 21:22:49 +0300

    REMOVE_FROM_BASKET feature

--- src/reducer.js
+++ src/reducer.js
@@ -15,9 +15,20 @@ switch(action.type) {
             basket: [...state.basket, action.item]
         };
     case 'REMOVE_FROM_BASKET':
-        return { ...state,
-             basket: state.basket.filter(item => item.id !== action.id)}
+        const index = state.basket.findIndex(
+            (basketItem) => basketItem.id === action.id);
+            let newBasket = [...state.basket]
 
+            if (index >= 0) 
+            {
+                newBasket.splice(index, 1)
+            } else{
+            console.warn(`Cant remove product (id: ${action.id}) as its not in the basket!`)
+        }
+        return {
+            ...state,
+            basket: newBasket
+        }
         default:
             return state;
 }
