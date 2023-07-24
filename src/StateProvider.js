6646c838cd15860996d1ec2a07ac9141b74508da
preston176 <prestonnyamweya@gmail.com>
2023-07-24 17:59:22 +0300

    StateProvider added

---
+++ src/StateProvider.js
@@ -0,0 +1,15 @@
+import { createContext, useContext, useReducer } from "react";
+// Prepares dataLayer
+export const StateContext = createContext();
+
+// Wrap our app and provide data layer
+
+export const StateProvider = ({ reducer, initialState, children}) => (
+    <StateContext.Provider value={useReducer(reducer, initialState)}>
+    {children}
+    </StateContext.Provider>
+)
+
+// get info from dataLayer
+
+export const useState = () => useContext(StateContext);
