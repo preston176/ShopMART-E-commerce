845d92409d19fef4dd07a5fd84a55156d1afe6ca
preston176 <prestonnyamweya@gmail.com>
2023-07-24 21:50:34 +0300

    pass and email states

--- src/Login.jsx
+++ src/Login.jsx
@@ -1,7 +1,11 @@
 import { Link } from 'react-router-dom'
 import './Login.css'
+import { useState } from 'react'
 
 const Login = () => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
   return (
     <div className="login">
     <Link to='/'>
@@ -13,9 +17,11 @@ const Login = () => {
 
             <form action="">
                 <h5>E-mail</h5>
-                <input type="text" />
+                <input type="text"  value={email}
+                    onChange={e => {setEmail(e.target.value)}}
+                />
                 <h5>Password</h5>
-                <input type="password" />
+                <input type="password" value={password} onChange={e => {setPassword(e.target.value)}}/>
 
                 <button className='login__signInButton'>Log In</button>
             </form>
