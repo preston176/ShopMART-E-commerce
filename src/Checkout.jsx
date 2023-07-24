32e8bba213218a14fe1dc17a1d84f0961e292b86
preston176 <prestonnyamweya@gmail.com>
2023-07-24 15:45:06 +0300

    checkout page layout

--- src/Checkout.jsx
+++ src/Checkout.jsx
@@ -3,7 +3,10 @@ import './checkout.css'
 const Checkout = () => {
   return (
     <div className='checkout'>
-      Checkout
+      <div className="checkout__left">
+        <img className='checkout__ad' src="..." alt="checkout__ad" />
+      </div>
+      <div className="checkout__right"></div>
     </div>
   )
 }
