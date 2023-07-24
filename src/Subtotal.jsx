31fe40aafee3e1a3e4ee199fc79f812db895839e
preston176 <prestonnyamweya@gmail.com>
2023-07-24 18:52:10 +0300

    getBasketTotal subtotal

--- src/Subtotal.jsx
+++ src/Subtotal.jsx
@@ -1,30 +1,34 @@
-import './subtotal.css'
-import CurrencyFormat from 'react-currency-format'
+import { useStateValue } from "./StateProvider";
+import { getBasketTotal } from "./reducer";
+import "./subtotal.css";
+import CurrencyFormat from "react-currency-format";
 
 const Subtotal = () => {
+  const [{ basket }, dispatch] = useStateValue();
+
   return (
-    <div className='subtotal'>
-    <CurrencyFormat
-    renderText={(value) => (
-        <div>
-        <p>
-            Subtotal (0 items):
-            <strong>{0}</strong>
+    <div className="subtotal">
+      <CurrencyFormat
+        renderText={(value) => (
+          <>
+            <p>
+              Subtotal ({basket.length} items):
+              <strong>{value}</strong>
             </p>
             <small className="subtotal__gift">
-                <input type="checkbox" /> This order contains a gift
+              <input type="checkbox" /> This order contains a gift
             </small>
-        </div>
-    )}
-      decimalScale={2}
-      value={0}
-    //   displayType={"Text"}
-      thousandSeparator={true}
-      prefix={"$"}
+          </>
+        )}
+        decimalScale={2}
+        value={getBasketTotal(basket)} // Replace this with the actual subtotal value
+        displayType={"text"}
+        thousandSeparator={true}
+        prefix={"$"}
       />
       <button>Proceed to Checkout</button>
     </div>
-  )
-}
+  );
+};
 
-export default Subtotal
+export default Subtotal;
