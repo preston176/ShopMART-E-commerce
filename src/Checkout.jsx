2bc45b0c04a3a9ba25bd7bedc192bb024e1d9826
preston176 <prestonnyamweya@gmail.com>
2023-07-24 15:52:52 +0300

    styling checkout section

--- src/Checkout.jsx
+++ src/Checkout.jsx
@@ -5,8 +5,18 @@ const Checkout = () => {
     <div className='checkout'>
       <div className="checkout__left">
         <img className='checkout__ad' src="..." alt="checkout__ad" />
+
+        <div>
+        <h2 className="checkout__title">
+            Your shopping Basket
+        </h2>
+        {/* basket items */}
+        </div>
       </div>
-      <div className="checkout__right"></div>
+
+      <div className="checkout__right">
+        <h2>The subtotal will go here</h2>
+      </div>
     </div>
   )
 }
