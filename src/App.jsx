1afda828e1d7792d802f8dae88665633d941df46
preston176 <prestonnyamweya@gmail.com>
2023-07-24 15:40:47 +0300

    checkout component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
+import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
 import Header from "./Header";
 import Home from "./Home";
+import Checkout from "./Checkout";
 
 const App = () => {
   return (
@@ -10,6 +12,7 @@ const App = () => {
         <Header />
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/checkout" element={<Checkout />} />
         </Routes>
       </div>
     </BrowserRouter>
---
+++ src/Checkout.jsx
@@ -0,0 +1,11 @@
+import './checkout.css'
+
+const Checkout = () => {
+  return (
+    <div className='checkout'>
+      Checkout
+    </div>
+  )
+}
+
+export default Checkout
