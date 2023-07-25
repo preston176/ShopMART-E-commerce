e026237b115c49275079c80018a8a99276097c78
preston176 <prestonnyamweya@gmail.com>
2023-07-25 14:15:22 +0300

    more products added

--- src/producsData.js
+++ src/producsData.js
@@ -57,10 +57,58 @@ export const products = [
       },
       {
         id: "8",
-        title: "Xiaomi Redmi K50 Gaming 5G + 4G LTE (256GB + 12GB) (NOT Verizon AT&T Cricket Straight Talk) NFC Triple Camera 6.67 + (w/Fast Car Charger Bundle) (Electric Auction)",
-        price: 569.99,
+        title: "SAMSUNG 980 PRO SSD 2TB",
+        price: 119.99,
         image:
-          "https://m.media-amazon.com/images/I/51U588+m-YL._AC_SX522_.jpg",
+          "https://m.media-amazon.com/images/I/71KeDAkw+0L._AC_SX300_SY300_.jpg",
+        rating: 4,
+      },
+      {
+        id: "9",
+        title: "Apple iPad (9th Generation)",
+        price: 256.68,
+        image:
+          "https://m.media-amazon.com/images/I/61NGnpjoRDL._AC_UY327_FMwebp_QL65_.jpg",
+        rating: 4,
+      },
+      {
+        id: "10",
+        title: "Apple Watch series 7 (GPS, 45mm)",
+        price: 314.95,
+        image:
+          "https://m.media-amazon.com/images/I/61HHeTRvb3L._AC_SX466_.jpg",
+        rating: 4,
+      },
+      {
+        id: "11",
+        title: "SAMSUNG 980 PRO SSD 2TB",
+        price: 119.99,
+        image:
+          "https://m.media-amazon.com/images/I/71KeDAkw+0L._AC_SX300_SY300_.jpg",
+        rating: 4,
+      },
+      {
+        id: "12",
+        title: "SAMSUNG 980 PRO SSD 2TB",
+        price: 119.99,
+        image:
+          "https://m.media-amazon.com/images/I/71KeDAkw+0L._AC_SX300_SY300_.jpg",
+        rating: 4,
+      },
+      {
+        id: "8",
+        title: "SAMSUNG 980 PRO SSD 2TB",
+        price: 119.99,
+        image:
+          "https://m.media-amazon.com/images/I/71KeDAkw+0L._AC_SX300_SY300_.jpg",
+        rating: 4,
+      },
+      {
+        id: "8",
+        title: "SAMSUNG 980 PRO SSD 2TB",
+        price: 119.99,
+        image:
+          "https://m.media-amazon.com/images/I/71KeDAkw+0L._AC_SX300_SY300_.jpg",
         rating: 4,
       },
   ];
