7bfc2dd803f60e7c549ad5432d9bb6a8be864a45
preston176 <prestonnyamweya@gmail.com>
2023-07-25 13:00:38 +0300

    done

--- functions/index.js
+++ functions/index.js
@@ -1,33 +1,34 @@
-const functions = require('firebase-functions');
+const functions = require("firebase-functions");
 
-const express = require('express');
-const cors = require('cors');
-const stripe = require('stripe')('sk_test_51NUHNtEFBkCVrySQOc98N7TLtO0xwvmoaPSH81tp4f2uuPokh4mhZm094M9MOYNUBaj1Z68MmU9nPt6Ks9mQxzd400IariYDsd');
-//API
+const express = require("express");
+const cors = require("cors");
+// eslint-disable-next-line max-len
+const stripe = require("stripe")("sk_test_51NUHNtEFBkCVrySQOc98N7TLtO0xwvmoaPSH81tp4f2uuPokh4mhZm094M9MOYNUBaj1Z68MmU9nPt6Ks9mQxzd400IariYDsd");
+// API
 
-//-App config
+// -App config
 const app = express();
 
-//-Middlewares
-app.use(cors({ origin: true}));
+// -Middlewares
+app.use(cors({origin: true}));
 app.use(express.json());
 
-//-API routes
-app.get('/', (request,response) => response.status(200).send('hello world'));
+// -API routes
+app.get("/", (request, response) => response.status(200).send("hello world"));
 
-app.post('/payments/create', async (request, response) => {
-    const total = request.query.total;
-    console.log('Payment Request Received', total);
-    const paymentIntent = await stripe.paymentIntents.create({
-      amount: total,
-      currency: "usd",
-    });
-    response.status(201).send({
-      clientSecret: paymentIntent.client_secret, // Corrected property name
-    });
+app.post("/payments/create", async (request, response) => {
+  const total = request.query.total;
+  console.log("Payment Request Received", total);
+  const paymentIntent = await stripe.paymentIntents.create({
+    amount: total,
+    currency: "usd",
   });
+  response.status(201).send({
+    clientSecret: paymentIntent.client_secret, // Corrected property name
+  });
+});
 
-//-Listener
+// -Listener
 
 
-exports.api = functions.https.onRequest(app)
+exports.api = functions.https.onRequest(app);
--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
 const instance = axios.create({
-    baseURL: 'http://127.0.0.1:5001/markethub-f6f5e/us-central1/api' // API url or cloud function
+    baseURL: 'https://us-central1-markethub-f6f5e.cloudfunctions.net/api' // API url or cloud function
 })
 
 export default instance
+
+// for debug baseURL: 'http://127.0.0.1:5001/markethub-f6f5e/us-central1/api' //
