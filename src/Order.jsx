7ed1fea7a214b1ecfc96ef04b5c50da59e7adc0e
preston176 <prestonnyamweya@gmail.com>
2023-07-25 06:02:41 +0300

    orders fixed

---
+++ src/Order.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import './Order.css';
+import moment from 'moment';
+import CheckoutProduct from './CheckoutProduct';
+import { FormattedNumber } from 'react-intl';
+
+const Order = ({ order }) => {
+  return (
+    <div className='order'>
+      <h2>Order ID: {order.id}</h2>
+      <p>Order Date: {moment.unix(order.data.created).format('MMMM Do YYYY, h:mma')}</p>
+      <p className='order__id'>
+        <small>{order.id}</small>
+      </p>
+
+      {order.data.basket?.map((item) => (
+        <CheckoutProduct
+          key={item.id}
+          id={item.id}
+          title={item.title}
+          image={item.image}
+          price={item.price}
+          rating={item.rating}
+          hideButton
+        />
+      ))}
+
+      <h3 className='order__total'>Order Total: 
+        <FormattedNumber
+          value={order.data.amount / 100}
+          style='currency'
+          currency='USD'
+        />
+      </h3>
+    </div>
+  );
+};
+
+export default Order;
--- src/Orders.jsx
+++ src/Orders.jsx
@@ -1,11 +1,49 @@
-import './Orders.css'
+import { useEffect, useState } from 'react';
+import { db, auth } from './firebase';
+import { collection, query, orderBy, onSnapshot } from 'firebase/firestore';
+import './Orders.css';
+import Order from './Order';
 
 const Orders = () => {
-  return (
-    <div className='orders'>
-      <h1>Your Orders</h1>
-    </div>
-  )
-}
+    const [orders, setOrders] = useState([]);
+    const user = auth.currentUser;
 
-export default Orders
+    useEffect(() => {
+        if (user) {
+            // Listen for changes in the 'orders' collection for the current user
+            const ordersQuery = query(
+                collection(db, 'users', user.uid, 'orders'),
+                orderBy('created', 'desc')
+            );
+
+            const unsubscribe = onSnapshot(ordersQuery, (snapshot) => {
+                setOrders(
+                    snapshot.docs.map((doc) => ({
+                        id: doc.id,
+                        data: doc.data(),
+                    }))
+                );
+            });
+
+            // Unsubscribe from the Firestore listener when the component unmounts
+            return () => unsubscribe();
+        }
+    }, [user]);
+
+    if (!user) {
+        return <p>Please sign in to view your orders.</p>;
+    }
+
+    return (
+        <div className='orders'>
+            <h1>Your Orders</h1>
+            {orders.length > 0 ? (
+                orders.map((order) => <Order key={order.id} order={order} />)
+            ) : (
+                <p>No orders found.</p>
+            )}
+        </div>
+    );
+};
+
+export default Orders;
