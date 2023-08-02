56f8c827dd58c1f803d1f54afa5ce7e17142c89e
preston176 <prestonnyamweya@gmail.com>
2023-08-02 03:43:37 +0300

    css fix

--- src/About.jsx
+++ src/About.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
+import Stats from './Stats'
+import Footer from './Footer'
+import { Container } from '@mui/material'
 
 const About = () => {
   return (
     <div>
-      
+      <Container>
+        <p className='text-slate-800 text-center h-2'> We are committed to providing you with a seamless shopping experience, from browsing to checkout, and beyond. Join us in exploring the latest trends, discovering exciting deals, and finding your next favorite item.</p>
+      </Container>
+      <div className="w-fit m-auto"><Stats /></div>
+      <div className="sticky-bottom">
+        <Footer />
+      </div>
     </div>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -5,16 +5,18 @@ import { IntlProvider } from "react-intl";
 import "./index.css";
 import reducer, { initialState } from "./reducer.js";
 import { StateProvider } from "./StateProvider";
+import 'mdb-react-ui-kit/dist/css/mdb.min.css';
+import "@fortawesome/fontawesome-free/css/all.min.css";
 
-const locale = "en"; // Replace with your desired locale code
-const messages = {}; 
+const locale = "en";
+const messages = {};
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-  <StateProvider initialState={initialState} reducer={reducer}>
-  <IntlProvider locale={locale} messages={messages}>
-    <App />
-    </IntlProvider>
+    <StateProvider initialState={initialState} reducer={reducer}>
+      <IntlProvider locale={locale} messages={messages}>
+        <App />
+      </IntlProvider>
     </StateProvider>
   </React.StrictMode>,
 );
